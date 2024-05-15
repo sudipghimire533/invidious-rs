6b4a82a8065cecb14518e3e2f3407a97caccb707
sudipghimire533 <sudipghimire533@gmail.com>
2024-05-15 15:27:11 +0200

    Remove web_client module

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,6 @@ pub mod endpoints_impl;
 pub mod instance;
 pub mod types;
 pub mod utils;
-pub mod web_client;
 
 pub mod reexports {
     pub use url;
--- src/web_client/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod error;
-pub mod get;
-pub mod post;
